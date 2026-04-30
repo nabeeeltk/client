9c889fb67d63ee6671c1b385a07329d2c0d5d6f4
apps@travelmythri.com <apps@travelmythri.com>
2026-04-30 09:50:15 +0530

    server

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://server-c6du4omry-nabeel-tks-projects.vercel.app',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:5001',
+        target: 'https://server-c6du4omry-nabeel-tks-projects.vercel.app',
         changeOrigin: true,
       }
     }
